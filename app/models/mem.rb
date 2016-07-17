05e0ee773e8641a8666069b3a85cef42f0f235f8
Piotr <pk63@onet.pl>
2016-07-17 13:09:53 +0000

    paperclip2

--- app/models/mem.rb
+++ app/models/mem.rb
@@ -2,6 +2,13 @@ class Mem < ActiveRecord::Base
     belongs_to:user
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
 :default_url => "/images/:style/missing.png"
+
+validates :name, presence: true
+
+after_validation(on: :create) do
+self.active = false
+end
+
 validates_attachment :image,
 :content_type => { :content_type => ["image/jpg","image/jpeg", "image/gif", "image/png"] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717121608) do
+ActiveRecord::Schema.define(version: 20160717125406) do
 
   create_table "mems", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160717121608) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.boolean  "active"
   end
 
   create_table "users", force: :cascade do |t|
